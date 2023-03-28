e1efcb4807899924a4599a6cf25c69d0eaf400e6
pautripodi <paulatripoddi@gmail.com>
2023-03-28 13:58:20 -0300

    Clase Javascript

---
+++ script.js
@@ -0,0 +1,56 @@
+alert("Hola mundo")
+
+let alumna = "Lucia";
+let profesor = "Juan Carlos";
+let edad = 20;
+
+console.log(" La alumna " + alumna + " tiene " + edad + " y tiene de profesor a " + profesor);
+
+let Alumno
+let _apellido
+let $edad
+let camelCase
+let nombreApellido = "Paula Tripodi"
+let nombre_apellido = "ana maria"
+let NombreApellido="paula tripodi"
+
+let entidad = "Gobierno buenos aires" 
+ entidad = "Nacion"
+
+console.log( "La entidad es :" + entidad) 
+
+////tipos de datos////
+//strings//
+let str1= "Strig con comillas dobles"
+let str2= 'String con comillas simples'
+let str3= `String con Backticks`
+
+let str4 = str1 + str2 
+
+console.log(str1 + str2)
+
+let str5 = `${str1} y ademas ${str2}`
+let str6 = str1 + "y ademas" + str2
+
+//Numeros//
+let nombre = prompt("Como te llamas?")
+let numero1 = prompt("mete el numero 1");
+let numero2 = prompt("mete el segundo numero");
+let resta = numero1-numero2
+let suma = numero1+numero2
+
+
+alert( "Hola " + nombre)
+alert( "La suma es:" + suma)
+
+console.log("calculadora")
+console.log( "la resta es : " + resta)
+
+
+//boleanos
+let verdadera = true
+let falsa = false
+
+let respuesta = 3>4
+console.log(respuesta)
+
